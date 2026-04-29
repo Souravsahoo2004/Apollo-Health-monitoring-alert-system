8741138859c7cbb8fda3d622afbd3147704be23a
sourav sahoo <souravsahoo72051@gmail.com>
2026-04-29 19:14:12 +0530

    Update Login, Register Navigations, Handel Otp APis

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
       [e.target.name]: e.target.value,
     });
   };
-
+const [showPassword, setShowPassword] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -89,23 +89,36 @@ export default function LoginPage() {
           </div>
 
           {/* PASSWORD */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              required
-              placeholder="Enter your password"
-              value={formData.password}
-              onChange={handleChange}
-              className="mt-1 w-full px-4 py-2 border rounded-lg 
-              text-gray-800 placeholder-gray-500 
-              focus:ring-2 focus:ring-[#2F4FA3] focus:border-[#2F4FA3] 
-              outline-none"
-            />
-          </div>
+         {/* PASSWORD */}
+<div>
+  <label className="block text-sm font-medium text-gray-700">
+    Password
+  </label>
+
+  <div className="relative mt-1">
+    <input
+      type={showPassword ? "text" : "password"}
+      name="password"
+      required
+      placeholder="Enter your password"
+      value={formData.password}
+      onChange={handleChange}
+      className="w-full px-4 py-2 border rounded-lg 
+      text-gray-800 placeholder-gray-500 
+      focus:ring-2 focus:ring-[#2F4FA3] focus:border-[#2F4FA3] 
+      outline-none pr-10"
+    />
+
+    {/* Toggle Button */}
+    <button
+      type="button"
+      onClick={() => setShowPassword(!showPassword)}
+      className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+    >
+      {showPassword ? "🙈" : "👁️"}
+    </button>
+  </div>
+</div>
 
           {/* Forgot Password */}
           <div className="flex justify-end">
